26bde36ff799f9d7af8a5cb68ff06165c06ce304
ssilveto <ssilveto@gmail.com>
2019-02-28 16:09:22 +0200

    My first working selenium code

---
+++ src/Selenium/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace MySelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var driver = new ChromeDriver("./"))
+            {
+                driver.Url = "file:///C:/_/projects/rojydesign/dogramaplovdiv.html";
+                Screenshot screenshot = driver.GetScreenshot();
+                screenshot.SaveAsFile("gg.png");
+            }
+        }
+    }
+}
