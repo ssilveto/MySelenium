2fbda726cc50b9563fb1b328db2d9958faf65f3b
ssilveto <ssilveto@gmail.com>
2019-03-05 14:38:23 +0200

    Save an image through Selenium WebDriver

---
+++ src/MySelenium/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace MySelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string currentDir = "./";
+            using (var driver = new ChromeDriver(currentDir))
+            {
+                driver.Url = "https://www.google.com";
+
+                Screenshot myScreenshot = driver.GetScreenshot();
+                myScreenshot.SaveAsFile("image.png");
+            }
+        }
+    }
+}
