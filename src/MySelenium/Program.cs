23039a9b4feafdba83aa0dc983f09f8a431f5e48
ssilveto <ssilveto@gmail.com>
2019-03-05 14:55:12 +0200

    Contains "view page source"

--- src/MySelenium/Program.cs
+++ src/MySelenium/Program.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.IO;
 
 namespace MySelenium
 {
@@ -9,12 +10,14 @@ namespace MySelenium
         static void Main(string[] args)
         {
             const string currentDir = "./";
-            string url = "https://google.com";
-
+            string url = "file:///C:/_/projects/rojydesign/dogramaplovdiv.html";
 
             using (var driver = new ChromeDriver(currentDir))
             {
                 driver.Url = url;
+
+                string pageSource = driver.PageSource;
+                File.WriteAllText("source.html", pageSource);
 
                 Screenshot myScreenshot = driver.GetScreenshot();
                 myScreenshot.SaveAsFile($"ss-{DateTime.UtcNow.ToString("yyyyMMdd-hhmmss-fff")}.png");
