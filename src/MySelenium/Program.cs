ddf0186063cd10c901043fd0855f00611aca4fd3
ssilveto <ssilveto@gmail.com>
2019-03-09 16:26:08 +0200

    Find link and Image

--- src/MySelenium/Program.cs
+++ src/MySelenium/Program.cs
@@ -12,11 +12,34 @@ namespace MySelenium
             const string currentDir = "./";
             string url = "file:///C:/_/projects/rojydesign/dogramaplovdiv.html";
 
-            using (var driver = new ChromeDriver(currentDir))
+            using (ChromeDriver driver = new ChromeDriver(currentDir))
             {
                 driver.Url = url;
+                IWebElement rojyLink = driver.FindElementByTagName("a");
+                string rojyLinkExtracted = rojyLink.GetAttribute("href");
+                string rojyLinkExpected = "http://rojy.design/";
+                if (rojyLinkExtracted == rojyLinkExpected)
+                {
+                    Console.WriteLine("Link");
+                    Console.ReadLine();
+                }
 
+
+
+                IWebElement rojyImage = driver.FindElementByTagName("img");
+
+                string extractedSrcValue = rojyImage.GetAttribute("src");
+                string expectedSrcValue = "http://rojy.design/wp-content/uploads/2017/01/rojy-logo-1.png";
+                if (rojyImage.Displayed && rojyImage.Enabled && expectedSrcValue == extractedSrcValue)
+                {
+                    Console.WriteLine("Tam e");
+                }
             }
+        }
+
+        static bool RojyImageExists(int f, int sec)
+        {
+            return true;
         }
     }
 }
